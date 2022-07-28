6b90d4f4175c344f30e200e3831c7d3558f933a6
Nicholas Punzalan <nicholas.punzalan@gmail.com>
2022-07-28 14:31:17 -0400

    lesson_2

---
+++ RB101/lesson_2/calculator.rb
@@ -0,0 +1,13 @@
+# ask the user for two numbers
+# ask the user for an operation to perform
+# perform the operation on the two numbers
+# output the result
+
+ 
+Kernel.puts("Welcome to Calculator!")
+
+Kernel.puts("What's the first numbner?")
+number1 = Kernel.gets()
+
+Kernel.puts(number1.chomp().inspect())
+
