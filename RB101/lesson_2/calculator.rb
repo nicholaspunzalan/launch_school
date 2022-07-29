dacee6e604b8bc75f1ef8e0a2cd41090e1913683
Nicholas Punzalan <nicholas.punzalan@gmail.com>
2022-07-29 14:34:13 -0400

    pseudocode.rb

--- RB101/lesson_2/calculator.rb
+++ RB101/lesson_2/calculator.rb
@@ -1,16 +1,28 @@
-# ask the user for two numbers
-# ask the user for an operation to perform
-# perform the operation on the two numbers
-# output the result
 
  
 Kernel.puts("Welcome to Calculator!")
 
+# ask the user for two numbers
 Kernel.puts("What's the first numbner?")
 number1 = Kernel.gets().chomp()
-
 Kernel.puts("What's the second number?")
 number2 = Kernel.gets().chomp()
 
-Kernel.puts(number1 + " " + number2)
+# ask the user for an operation to perform
+Kernel.puts("What operation would you like to perform? 1) add 2) subtract 3) multiply 4) divide")
+operator = Kernel.gets().chomp()
+
+# perform the operation on the two numbers
+if operator == '1'
+  result = number1.to_i() + number2.to_i()
+elsif operator == '2'
+  result = number1.to_i() - number2.to_i()
+elsif operator == '3'
+  result = number1.to_i() * number2.to_i()
+elsif operator == '4'
+  result = number1.to_f() / number2.to_f()
+end
+
+# output the result
+Kernel.puts("The result is #{result}")
 
---
+++ RB101/lesson_2/pseudocode.rb
@@ -0,0 +1,26 @@
+# a method that returns the sum of two integers
+
+# prompt ouser for 2 integers
+# add 2 iontegers
+# return oresult
+
+START
+# prompt ouser for 2 integers
+GET user input for first number
+SET num1 = user input
+GET user input for first number
+SET num1 = user input
+
+# add 2 iontegers
+SET result = num1 + num2
+
+# return oresult
+RETURN result
+
+puts ">> Pick your first number."
+num1 = gets.chomp
+puts ">> Pick your second number."
+num2 = gets.chomp
+
+result = num1 + num2
+
