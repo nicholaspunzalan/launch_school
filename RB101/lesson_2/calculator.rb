9cd3f2fc1e26acd2d4e3bb3df2e46322245e41fe
Nicholas Punzalan <nicholas.punzalan@gmail.com>
2022-07-29 10:48:13 -0400

    calculator.rb

--- RB101/lesson_2/calculator.rb
+++ RB101/lesson_2/calculator.rb
@@ -7,7 +7,10 @@
 Kernel.puts("Welcome to Calculator!")
 
 Kernel.puts("What's the first numbner?")
-number1 = Kernel.gets()
+number1 = Kernel.gets().chomp()
 
-Kernel.puts(number1.chomp().inspect())
+Kernel.puts("What's the second number?")
+number2 = Kernel.gets().chomp()
+
+Kernel.puts(number1 + " " + number2)
 
