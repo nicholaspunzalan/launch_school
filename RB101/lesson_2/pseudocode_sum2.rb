f2ac611f0a0b5fc7a03e056cddd8fff13a0d9233
nicholas punzalan <dad@nicholass-imac.lan>
2022-07-29 16:32:20 -0400

    updating repository

---
+++ RB101/lesson_2/pseudocode_sum2.rb
@@ -0,0 +1,29 @@
+# a method that returns the sum of two integers
+
+# prompt ouser for 2 integers
+# add 2 iontegers
+# return oresult
+
+#START
+## prompt ouser for 2 integers
+#GET user input for first number
+#SET num1 = user input
+#GET user input for first number
+#SET num1 = user input
+#
+## add 2 iontegers
+#SET result = num1 + num2
+#
+## return result
+#RETURN result
+
+def sum2()
+  puts ">> Pick your first number."
+  num1 = gets.chomp
+  puts ">> Pick your second number."
+  num2 = gets.chomp
+
+  result = num1.to_i + num2.to_i
+end
+
+p sum2
---
+++ RB101/lesson_2/string.rb
@@ -0,0 +1,37 @@
+
+
+#Make an array of a bunch of strings
+#iterate over array
+#- save first value as starting value
+#- concatenate subsequent value to starting value
+#- save new value
+#
+#After iterating over array, return the saved value
+
+#START
+#SET array = ["bunch", "o", "strings"]
+#SET iterator = 0
+#SET value = String.new
+#WHILE iterator < array.length
+#  SET value = value + array[iterator]
+#
+#print value
+#
+#END
+
+def concaterminator
+  array = ["bunch", "o", "strings"]
+
+  iterator = 0
+  value = String.new()
+
+  while iterator < array.length()
+    value = value + array[iterator] 
+    iterator += 1
+  end
+
+  return value
+
+end
+
+p concaterminator
